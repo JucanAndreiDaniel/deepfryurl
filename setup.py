133082bd5c204cb4caafca8c28ec11c0646352dc
Andrew <andrew@pop-os.localdomain>
2020-11-13 01:44:26 +0200

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='deepfryurl',
     version='0.1.0',
     description='Deepfry an image from url',
-    url='https://github.com/shuds13/pyexample',
+    url='https://github.com/JucanAndreiDaniel/deepfryurl',
     author='Jucan Andrei Daniel',
     author_email='andrei.jucan00@e-uvt.ro',
     packages=['deepfryurl'],
